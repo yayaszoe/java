8a1a5ca7bae2715dcab188529e43f40b9e4f03d1
yayaszoe <129402512+yayaszoe@users.noreply.github.com>
2024-02-29 08:46:08 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
-const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-02-20T00:00:00");
-let tempoAtual = new Date();
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
 
